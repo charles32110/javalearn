da717f48194d5bf47decc3d7eb2d3e8b04499f24
charles32110 <596147986@qq.com>
2019-01-24 23:58:17 +1300

    for&while

---
+++ src/learn6.java
@@ -0,0 +1,21 @@
+/*
+for while
+ */
+
+
+
+public class learn6 {
+    static public void main(String [] agrs){
+        for (int i = 1; i<=10; i++){
+            System.out.println("get one thing as for");
+            System.out.println(i);
+        }
+        int o = 1;
+        while (o<10){
+            System.out.println("get another thing as while");
+            System.out.println(o);
+            o++;
+
+        }
+    }
+}
