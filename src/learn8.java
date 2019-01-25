aa8329ee1251fe355c0e1fe3d787f62cc4a46b89
charles32110 <596147986@qq.com>
2019-01-25 20:00:27 +1300

    break& continue

--- src/learn8.java
+++ src/learn8.java
@@ -1,2 +1,24 @@
+/*
+break
+continue
+ */
+
+
 public class learn8 {
+    public static void main(String[] args){
+        for(int i = 1;i < 10;i++){
+            /*
+            if (i % 3 == 0){
+                System.out.println("I still like python");
+                break;
+            }*/
+            if (i % 3 == 0) {
+                System.out.println("I still like python");
+                continue;//jump out of the circulation and continue to the next terminal
+            }
+            System.out.println("hello java");
+
+        }
+
+    }
 }
