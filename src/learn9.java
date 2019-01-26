ef6b8f8becdf1db8987ac7384d6d7b21b703453c
charles32110 <596147986@qq.com>
2019-01-26 22:50:07 +1300

    new function

---
+++ src/learn9.java
@@ -0,0 +1,24 @@
+
+/*
+call function
+
+ */
+
+
+public class learn9 {
+    public static void main(String[] args) {
+        int ff = suma(10,21);
+        System.out.print(ff);
+
+    }
+    public static int suma(int a, int b){
+        int sum =  a + b ;
+        return sum;
+        }
+    
+
+
+
+
+
+}
