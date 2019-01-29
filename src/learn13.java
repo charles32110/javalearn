c3a06069735039537f2a845a0de025a3e4f14dc6
charles32110 <596147986@qq.com>
2019-01-29 16:45:21 +1300

    class

--- src/learn13.java
+++ src/learn13.java
@@ -1,15 +1,29 @@
 
 /*
-values
+rubbish recycle
+
+cfreslease(values)
  */
 
 
 
 public class learn13 {
     public static void main(String[] args){
-
+        Person p = new Person();
+        p.name = "jack";
+        p.age = 35;
+        int a = 10;//must give the value
+        p.speak();
 
 
 
     }
 }
+class Person{
+        String name;//member's value  default: 0
+        int age;
+        public void speak(){
+            System.out.println(name+age);
+        }
+    }
+
