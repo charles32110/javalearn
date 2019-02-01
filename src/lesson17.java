2875d0721419c62c75b6584ce4c4a705b3964c6a
charles32110 <596147986@qq.com>
2019-02-01 22:54:59 +1300

    true:false

---
+++ src/lesson17.java
@@ -0,0 +1,16 @@
+
+/*
+function  call
+true:false
+
+ */
+
+public class lesson17 {
+    public static void main(String[] args){
+        boolean b = 2>3? true:false;
+        System.out.println(b);
+
+    }
+
+
+}
